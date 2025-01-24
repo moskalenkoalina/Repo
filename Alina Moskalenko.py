3eb16806e89edd000ad406515a2fb0bdbee54819
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-24 21:59:56 +0200

    Додано першу функцію

--- Alina Moskalenko.py
+++ Alina Moskalenko.py
@@ -4,27 +4,3 @@ def parse_text() :
         t = file.read()
     words = re.findall(r'\b\w+\b', t.lower())
     return words
-
-def count_word(words):
-    word_counts = {}
-
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-
-    return word_counts
-
-def sort_and_display(word_counts):
-    sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
-    for word, count in sorted_words:
-        print(f"{word}: {count}")
-
-def main():
-    file_path = 'text.txt'
-    words = parse_text()
-    word_counts = count_word(words)
-    sort_and_display(word_counts)
-if __name__ == "__main__":
-    main()
