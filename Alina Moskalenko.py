06d2f2e325e15ecdbe753fdcfa3eda76f2e77b96
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-24 21:25:44 +0200

    Додаємо третю функцію

--- Alina Moskalenko.py
+++ Alina Moskalenko.py
@@ -4,3 +4,14 @@ def parse_text() :
         t = file.read()
     words = re.findall(r'\b\w+\b', t.lower())
     return words
+
+def count_word(words):
+    word_counts = {}
+
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    return word_counts
