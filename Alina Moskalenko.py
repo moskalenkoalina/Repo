2608beefe07e28588de59687fad876ca07220dcc
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-24 21:23:35 +0200

    Додано другу функцію

---
+++ Alina Moskalenko.py
@@ -0,0 +1,6 @@
+import re
+def parse_text() :
+    with open('text.txt', 'r', encoding='windows-1251') as file:
+        t = file.read()
+    words = re.findall(r'\b\w+\b', t.lower())
+    return words
