04fa2b02f7ec154eeadbe513923930755e1bf0fd
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-24 21:49:01 +0200

    Додано першу функцію

--- Alina Moskalenko.py
+++ Alina Moskalenko.py
@@ -6,17 +6,4 @@ def parse_text() :
     return words
 
 def count_word(words):
-    word_counts = {}
-
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-
-    return word_counts
-
-def sort_and_display(word_counts):
-    sorted_words = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
-    for word, count in sorted_words:
-        print(f"{word}: {count}")
+    word_counts = {}  
