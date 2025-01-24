40c2f2d5ed938f452763e8187b98fdfcc7ef1b70
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-24 21:51:20 +0200

    Додано другу функцію

--- Alina Moskalenko.py
+++ Alina Moskalenko.py
@@ -6,4 +6,12 @@ def parse_text() :
     return words
 
 def count_word(words):
-    word_counts = {}  
+    word_counts = {}
+
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+    return word_counts
