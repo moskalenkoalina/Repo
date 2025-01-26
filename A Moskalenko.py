267b90e4da5fb61e660a93b194f609b633c119fe
moskalenkoalina <moskalenkoalina062@gmail.com>
2025-01-26 20:43:10 +0200

    Змінено першу функцію

--- A Moskalenko.py
+++ A Moskalenko.py
@@ -1,8 +1,9 @@
-import re
-def parse_text() :
+def parse_text():
     with open('text.txt', 'r', encoding='windows-1251') as file:
         t = file.read()
-    words = re.findall(r'\b\w+\b', t.lower())
+    t = ''.join(char if char.isalnum() or char.isspace() else ' ' for char in t).lower()
+    words = t.split()
+
     return words
 
 def count_word(words):
